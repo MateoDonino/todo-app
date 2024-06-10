ea9c5745fa6094fc1c2d624760b401e722467dc0
mateodonino <mateodonino@gmail.com>
2024-06-10 18:36:26 -0300

    v1

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { FaPlus } from "react-icons/fa6";
+
+const AddTask = () => {
+  return (
+    <div>
+      <button className="btn btn-primary w-full">
+        Add New Task
+        <FaPlus size={16} />
+      </button>
+    </div>
+  );
+};
+
+export default AddTask;
