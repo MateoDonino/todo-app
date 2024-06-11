c16e733f560cafeececc07c0b8b0730a29ebd4ff
mateodonino <mateodonino@gmail.com>
2024-06-11 13:24:45 -0300

    v1.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 import { getAllTodos } from "@/api";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
+import Modal from "./components/Modal";
 
 export default async function Home() {
   const tasks = await getAllTodos();
   console.log(tasks);
 
   return (
-    <main className="max-w-4xl mx-auto mt-5">
+    <main className="min-h-screen max-w-4xl mx-auto px-2">
       <div className="text-center my-5 flex flex-col gap-4 ">
         <h1 className="text-2xl font-bold">Todo List App</h1>
         <AddTask />
       </div>
       <TodoList tasks={tasks} />
+      <Modal />
     </main>
   );
 }
