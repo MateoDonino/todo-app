167d57e51215661e17d2f5ec2ba7f2d126c84f72
mateodonino <mateodonino@gmail.com>
2024-07-02 17:12:22 -0300

    v1.3

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,13 +1,24 @@
+"use client";
 import React from "react";
 import { FaPlus } from "react-icons/fa6";
+import Modal from "./Modal";
+import { useState } from "react";
 
 const AddTask = () => {
+  const [modalOpen, setModalOopen] = useState<boolean>(false);
+
   return (
     <div>
-      <button className="btn btn-primary w-full text-lg">
+      <button
+        onClick={() => setModalOopen(true)}
+        className="btn btn-primary w-full text-lg"
+      >
         Add New Task
         <FaPlus size={16} />
       </button>
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOopen}>
+        modal for add todo
+      </Modal>
     </div>
   );
 };
