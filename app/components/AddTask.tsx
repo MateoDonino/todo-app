b928558a0355b83889ab05adaba5f64ed65c25ba
mateodonino <mateodonino@gmail.com>
2024-07-02 17:23:52 -0300

    v1.3

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,11 +1,19 @@
 "use client";
-import React from "react";
+import React, { FormEventHandler } from "react";
 import { FaPlus } from "react-icons/fa6";
 import Modal from "./Modal";
 import { useState } from "react";
+import { addTodo } from "../../api";
 
 const AddTask = () => {
   const [modalOpen, setModalOopen] = useState<boolean>(false);
+
+  const [newTaskValue, setNewTaskValue] = useState<string>("");
+  const handleSumbitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await addTodo({ id: 3, text: newTaskValue });
+    setNewTaskValue("");
+  };
 
   return (
     <div>
@@ -17,7 +25,21 @@ const AddTask = () => {
         <FaPlus size={16} />
       </button>
       <Modal modalOpen={modalOpen} setModalOpen={setModalOopen}>
-        modal for add todo
+        <form onSubmit={handleSumbitNewTodo}>
+          <h3 className="text-lg font-bold">Add New Task</h3>{" "}
+          <div className="modal-action">
+            <input
+              value={newTaskValue}
+              onChange={(e) => setNewTaskValue(e.target.value)}
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full"
+            />
+            <button className="btn" type="submit">
+              Sumbit
+            </button>
+          </div>
+        </form>
       </Modal>
     </div>
   );
